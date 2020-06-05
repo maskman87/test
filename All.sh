1803d1989f2647acf16bd01451b7287d7f201b03
maskman87 <taftazani.elhaff@gmail.com>
2020-06-05 07:24:53 +0700

    testbot

---
+++ All.sh
@@ -0,0 +1,73 @@
+python bot.py +14142366638 
+
+python bot.py +14147929546  
+
+python bot.py +14142539412  
+
+python bot.py +16626182666  
+
+python bot.py +19514562580  
+
+python bot.py +14022638013  
+
+python bot.py +13184313876  
+
+python bot.py +14063504794  
+
+python bot.py +14146167046  
+
+python bot.py +14144337164  
+
+python bot.py +15712865093  
+
+python bot.py +14842183514  
+
+python bot.py +6285777557888  
+
+python bot.py +14122127925  
+
+python bot.py +14148421613  
+
+python bot.py +14148421805  
+
+python bot.py +19164268607  
+
+python bot.py +14403688112  
+
+python bot.py +12522387703  
+
+python bot.py +12512742578  
+
+python bot.py +19787543394  
+
+python bot.py +14197578756  
+
+python bot.py +6285843555505  
+
+python bot.py +6285777557888 
+
+python bot.py +17312547917  
+
+python bot.py +6282235666676  
+
+python bot.py +6285868888645  
+
+python bot.py +6287704756276  
+
+python bot.py +14132252073  
+
+python bot.py +18643014362  
+
+python bot.py +13042237350  
+
+python bot.py +17242622446  
+
+python bot.py +12348135936  
+
+python bot.py +14142366565  
+
+python bot.py +17706158950  
+
+python bot.py +6285777557888 
+
+sh all.sh
